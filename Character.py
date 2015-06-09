a6461a435151bc264ea8837a186e51b6e1ba7b25
David Klumpenhower <dklump@gmail.com>
2015-06-09 13:43:19 -0400

    Character.py class holds functions for creating and storing characters

---
+++ Character.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#Created by David Klumpenhower
+#Created May 14, 2015
+#Character module
+import glob
+import os
+
+class Character:
+    'Common base class for all Characters'
+
+    def __init__(self, name, age, strength, constitution, wisdom, inteligence, charisma):
+        self.name = name 
+        self.age = age
+
+        self.strength = strength
+        self.constitution = constitution
+        self.wisdom = wisdom
+        self.inteligence = inteligence
+        self.charisma = charisma
+
+   
+def createCharacter(name, age, strength, constitution, wisdom, inteligence, charisma):
+    charName = Character(name, age, strength, constitution, wisdom, inteligence, charisma) 
+    return charName
+
+
+def saveCharacter(character):
+    derp=1
+
+
+
+def characterQuery():
+    characterNames = []
+    for characters in glob.glob('./Characters/*.char'):
+        characterNames.append(os.path.basename(characters))
+        return characterNames

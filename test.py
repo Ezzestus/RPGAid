3d327b8a4fd0bb45cc8aeb1f7a20192cc3dc3642
David Klumpenhower <dklump@gmail.com>
2015-06-23 18:20:07 -0400

    character test module

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#Created by David Klumpenhower
+#Created May 13, 2015
+#Dice Number Generation Python Script module test
+
+import diceRoll
+
+diceRoll = diceRoll.diceRoller (20)
+
+print (diceRoll)
